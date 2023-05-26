e34b6ce27658041c43eaf3539d6b0e9ba56de46c
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-05-26 22:52:12 +0200

    Add: create type model and relations

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -5,12 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Project extends Model
+class Type extends Model
 {
     use HasFactory;
-    protected $guarded = ['image', 'slug'];
 
-    public function type(){
-        return $this->belongsTo(Type::class);
+    public function projects(){
+        return $this->hasMany(Project::class);
     }
 }
